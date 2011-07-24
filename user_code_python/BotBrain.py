8ee884d0a948ba17a17bb5f275b9854bae6c235b
Y Devesh <dev344@gmail.com>
2011-07-24 15:48:38 +0530

    Added BotBrain.py in user code for python

---
+++ user_code_python/BotBrain.py
@@ -0,0 +1,25 @@
+class BotBrain :
+    """ Literally this is the brain of the bot.
+
+    @author Devesh Yamparala
+
+    This is the class whose functions players need 
+    to code. """
+
+    # Some static variables here if needed
+    dummy
+
+    def __init__(self, init_map, max_map_size) :
+        """ Constructor for the bot. 
+
+        @param init_map Char[][]: Inital map
+        @param max_map_size  Int: Max rows or columns in map.
+        """
+        self.map = init_map
+        self.map_size = max_map_size
+        
+    def play_move(self):
+        """ Function to decide which move to play.
+        """
+        pass
+
