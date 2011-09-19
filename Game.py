6f5baadfcbb0ecec573e5fae6e5ee39e8730b8ea
Sujeet Gholap <sujeetgholap@gmail.com>
2011-09-19 20:10:24 +0530

    Now the game makes sure that before crashing, all bot processes are killed.

--- Game.py
+++ Game.py
@@ -3,20 +3,31 @@ import os
 
 from Constants import *
 from Arena import *
+
     
 if __name__ == "__main__" :
     map_file_name = "map.txt"
     arena = Arena (map_file_name, sys.argv [1:])
 
-    for i in xrange (N_TURNS) :
-        arena.check_for_bot_crash ()
-        arena.get_moves ((i == 0))
-        arena.make_moves ()
-        arena.update_powers ()
-        arena.turn_count += 1
-        arena.map.log_updates (0, 0, "$", "$")  
-        if arena.game_over == True :
-            break
-
-    arena.terminate_game ()
-    os.system ("python log_to_html.py")
+    # a desperate attempt to avoud zombies :
+    try :
+        for i in xrange (N_TURNS) :
+            arena.check_for_bot_crash ()
+            arena.get_moves ((i == 0))
+            arena.make_moves ()
+            arena.update_powers ()
+            arena.turn_count += 1
+            arena.map.log_updates (0, 0, "$", "$")  
+            if arena.game_over == True :
+                break
+            
+        arena.terminate_game ()
+        os.system ("python log_to_html.py")
+        
+    except :
+        for bike in arena.bikes :
+            try :
+                bike.bot.process.kill ()
+            except OSError :
+                # the process was already terminated
+                pass
